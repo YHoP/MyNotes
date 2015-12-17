65e436d03077d93139a6ecaa683dc4732347b070
YHop <ypeng.tw@gmail.com>
2015-12-17 06:46:06 -0800

    Change private variables to public, modify insert, delete and update methods

--- app/src/main/java/com/example/yhop/mynotes/NotesProvider.java
+++ app/src/main/java/com/example/yhop/mynotes/NotesProvider.java
@@ -7,37 +7,40 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 
-
 public class NotesProvider extends ContentProvider{
 
     private static final String AUTHORITY = "com.example.yhop.mynotes.notesprovider";
     private static final String BASE_PATH = "notes";
-    public static Uri CONTENT_URI = Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH);
+    public static final Uri CONTENT_URI =
+            Uri.parse("content://" + AUTHORITY + "/" + BASE_PATH );
 
-    // constant to identify the requested operation
+    // Constant to identify the requested operation
     private static final int NOTES = 1;
     private static final int NOTES_ID = 2;
 
-    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+    private static final UriMatcher uriMatcher =
+            new UriMatcher(UriMatcher.NO_MATCH);
 
     static {
         uriMatcher.addURI(AUTHORITY, BASE_PATH, NOTES);
         uriMatcher.addURI(AUTHORITY, BASE_PATH +  "/#", NOTES_ID);
     }
 
-    SQLiteDatabase database;
+    private SQLiteDatabase database;
 
     @Override
     public boolean onCreate() {
+
         DBOpenHelper helper = new DBOpenHelper(getContext());
         database = helper.getWritableDatabase();
-        return false;
+        return true;
     }
 
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
-
-        return null;
+        return database.query(DBOpenHelper.TABLE_NOTES, DBOpenHelper.ALL_COLUMNS,
+                selection, null, null, null,
+                DBOpenHelper.NOTE_CREATED + " DESC");
     }
 
     @Override
@@ -47,16 +50,19 @@ public class NotesProvider extends ContentProvider{
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
-        return null;
+        long id = database.insert(DBOpenHelper.TABLE_NOTES,
+                null, values);
+        return Uri.parse(BASE_PATH + "/" + id);
     }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
+        return database.delete(DBOpenHelper.TABLE_NOTES, selection, selectionArgs);
     }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
-        return 0;
+        return database.update(DBOpenHelper.TABLE_NOTES,
+                values, selection, selectionArgs);
     }
 }
