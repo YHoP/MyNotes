ab4aa69f80118584fe93c86b4481aef8d71110e9
YHop <ypeng.tw@gmail.com>
2015-12-17 06:22:51 -0800

    Modify the NoteCursorAdapter, inflate the newView and bind the new note textView

--- app/src/main/java/com/example/yhop/mynotes/NotesCursorAdapter.java
+++ app/src/main/java/com/example/yhop/mynotes/NotesCursorAdapter.java
@@ -2,18 +2,32 @@ package com.example.yhop.mynotes;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.TextView;
 
 public class NotesCursorAdapter extends CursorAdapter{
+
+    public NotesCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return null;
+        return LayoutInflater.from(context).inflate(R.layout.note_list_item, parent, false);
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
+        String noteText = cursor.getString(cursor.getColumnIndex(DBOpenHelper.NOTE_TEXT));
+        int position = noteText.indexOf(10);
+        if (position != -1){
+            noteText = noteText.substring(0, position) + " ...";
+        }
 
+        TextView textView = (TextView) view.findViewById(R.id.textViewNote);
+        textView.setText(noteText);
     }
 }
